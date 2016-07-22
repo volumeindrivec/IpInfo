ffe02bf6f4fda307da387628aedee5f7eac82304
sukottosan <treebeard@id10tlabs.com>
2016-07-22 11:58:54 -0400

    Initial Commit.

---
+++ Get-IpInfo.ps1
@@ -0,0 +1,25 @@
+﻿function Get-IpInfo {
+    [cmdletbinding()]
+
+    param(
+        [Parameter(Mandatory=$True)][string[]]$IpAddress
+    )
+
+
+    
+    foreach ($Ip in $IpAddress){
+        try{
+            $Result = Invoke-WebRequest -Uri http://ipinfo.io/$Ip/json
+            $Content = $Result.Content
+            $Obj = $Content | ConvertFrom-Json
+
+            Write-Output $Obj
+        }
+        catch{
+            $Code = $_.Exception.Response.StatusCode.Value__
+            Write-Verbose "Error returned.  Code:  $Code"
+            #Write-Error "$_.Exception"
+        }
+    }
+
+}
