3cc52a72389f930442d53b38f25f389611809ce7
sukottosan <treebeard@id10tlabs.com>
2016-07-27 20:25:06 -0400

    Code refactoring.  Fixed some obvious issues that weren't caught the first time.

--- Get-IpInfo.ps1
+++ Get-IpInfo.ps1
@@ -1,34 +1,26 @@
 ﻿function Get-IpInfo {
-    [cmdletbinding()]
+  [cmdletbinding()]
 
-    param(
-        [string[]]$IpAddress
-    )
+  param(
+    [string[]]$IpAddress
+  )
 
 
-    if ($IpAddress){
-        foreach ($Ip in $IpAddress){
-        try{
-            $Result = Invoke-WebRequest -Uri http://ipinfo.io/$Ip/json
-            $Content = $Result.Content
-            $Obj = $Content | ConvertFrom-Json
-
-            Write-Output $Obj
-        }
-        catch{
-            $Code = $_.Exception.Response.StatusCode.Value__
-            Write-Verbose "Error returned.  Code:  $Code"
-            #Write-Error "$_.Exception"
-        }
+  if ($IpAddress){
+    foreach ($Ip in $IpAddress){
+      try{
+        $Content = (Invoke-WebRequest -Uri http://ipinfo.io/$Ip/json).Content | ConvertFrom-Json
+        Write-Output $Content
+      }
+      catch{
+        $Code = $_.Exception.Response.StatusCode.Value__
+        Write-Verbose "Error returned.  Code:  $Code"
+        #Write-Error "$_.Exception"
+      }
     }
-    }
-    else{
-        $Result = Invoke-WebRequest -Uri http://ipinfo.io
-        $Content = $Result.Content
-        $Obj = $Content | ConvertFrom-Json
-
-        Write-Output $Obj
-    }
-
-
+  }
+  else{
+    $Content = (Invoke-WebRequest -Uri http://ipinfo.io).Content | ConvertFrom-Json
+    Write-Output $Content
+  }
 }
