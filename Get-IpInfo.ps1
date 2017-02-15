0a941641de1d45e07b2dfd880bf6a2c26dec9235
volumeindrivec <treebeard@id10tlabs.com>
2017-02-15 13:56:01 -0500

    Changed code to use Invoke-RestMethod.

--- Get-IpInfo.ps1
+++ Get-IpInfo.ps1
@@ -2,26 +2,25 @@
   [cmdletbinding()]
 
   param(
-    [string[]]$IpAddress
+    [AllowNull()][string[]]$IpAddress
   )
 
 
   if ($IpAddress){
     foreach ($Ip in $IpAddress){
       try{
-        $Content = (Invoke-WebRequest -Uri http://ipinfo.io/$Ip/json).Content | ConvertFrom-Json
+        $Content = Invoke-RestMethod -Uri http://ipinfo.io/$Ip
         $Content | Add-Member -MemberType NoteProperty -Name 'dateretrieved' -Value (Get-Date -Format s) 
         Write-Output $Content
       }
       catch{
         $Code = $_.Exception.Response.StatusCode.Value__
-        Write-Verbose "Error returned.  Code:  $Code"
-        #Write-Error "$_.Exception"
+        Write-Verbose ('Error returned.  Code:  {0}' -f $Code)
       }
     }
   }
   else{
-    $Content = (Invoke-WebRequest -Uri http://ipinfo.io).Content | ConvertFrom-Json
+    $Content = Invoke-RestMethod -Uri http://ipinfo.io/$Ip
     $Content | Add-Member -MemberType NoteProperty -Name 'dateretrieved' -Value (Get-Date -Format s)
     Write-Output $Content
   }
